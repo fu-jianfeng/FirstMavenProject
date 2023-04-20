4fc865d2c32edea502fc704862cfabcb2a0595a9
fujianfeng <15779500385@139.com>
2023-04-20 19:11:14 +0800

    合并代码冲突已收到合并修改代码

--- src/main/java/com/atguigu/maven/Hello.java
+++ src/main/java/com/atguigu/maven/Hello.java
@@ -2,7 +2,7 @@ package com.atguigu.maven;
 
 public class Hello{
 	public String sayHello(String name){
-		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "master修改引发冲突";
-		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "20230420分支修改代码引发冲突";
+		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "master修改引发冲突" +
+		 "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "20230420分支修改代码引发冲突";
 	}
 }

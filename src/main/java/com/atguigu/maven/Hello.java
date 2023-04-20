298b604be97f0f1baa5f071fbcd0c8a807a8e077
fujianfeng <15779500385@139.com>
2023-04-20 19:00:50 +0800

    master分支修改代码

--- src/main/java/com/atguigu/maven/Hello.java
+++ src/main/java/com/atguigu/maven/Hello.java
@@ -2,6 +2,6 @@ package com.atguigu.maven;
 
 public class Hello{
 	public String sayHello(String name){
-		return "Hello " + name + "!" + "新分支" + "新分支第二次提交";
+		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "master修改引发冲突";
 	}
 }

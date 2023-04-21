db2d054c6ba6f277bf24da7be04bf9d066376cc6
fujianfeng <15779500385@139.com>
2023-04-21 12:47:53 +0800

    将本地代码push到远程仓库

--- src/main/java/com/atguigu/maven/Hello.java
+++ src/main/java/com/atguigu/maven/Hello.java
@@ -3,6 +3,7 @@ package com.atguigu.maven;
 public class Hello{
 	public String sayHello(String name){
 		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "master修改引发冲突" +
-		 "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "20230420分支修改代码引发冲突";
+		 "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "20230420分支修改代码引发冲突"
+				+ "在IDEA中集成GitHub后将本地代码push到远程仓库";
 	}
 }

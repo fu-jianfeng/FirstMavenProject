0ab6c8cc6fafecb6437c46ad383c9b381910d36b
fu-jianfeng <130981505+fu-jianfeng@users.noreply.github.com>
2023-04-21 12:55:24 +0800

    Update Hello.java
    将远程库中的代码拉取到本地

--- src/main/java/com/atguigu/maven/Hello.java
+++ src/main/java/com/atguigu/maven/Hello.java
@@ -4,6 +4,7 @@ public class Hello{
 	public String sayHello(String name){
 		return "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "master修改引发冲突" +
 		 "Hello " + name + "!" + "新分支" + "新分支第二次提交" + "20230420分支修改代码引发冲突"
-				+ "在IDEA中集成GitHub后将本地代码push到远程仓库";
+				+ "在IDEA中集成GitHub后将本地代码push到远程仓库"
+			+ "在IDEA中集成GitHub后将远程仓库中的代码拉取到本地";
 	}
 }
